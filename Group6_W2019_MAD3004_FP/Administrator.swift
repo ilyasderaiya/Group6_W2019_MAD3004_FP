8020f3518a50a380efbfa4d12b070af02bb225cb
MacStudent <macstudent@126-iMac38.local>
2019-02-19 18:32:05 -0500

    Created Classes

---
+++ Group6_W2019_MAD3004_FP/Administrator.swift
@@ -0,0 +1,9 @@
+//
+//  Administrator.swift
+//  Group6_W2019_MAD3004_FP
+//
+//  Created by MacStudent on 2019-02-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
