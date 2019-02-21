b68a78cb6db6f2a8d4715d8c108357b041141901
Ilyas <ilyasderaiya@gmail.com>
2019-02-21 18:50:14 -0500

    Added Var and Method in Shipping Info Class

--- Group6_W2019_MAD3004_FP/ShippingInfo.swift
+++ Group6_W2019_MAD3004_FP/ShippingInfo.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class ShippingInfo{
+    var shippingId: Int
+    var shippingType: String
+    var shippingCost: Float
+    var shippingRegionId: String
+    
+    init(){
+        self.shippingId = 0
+        self.shippingType = String()
+        self.shippingCost = 0.0
+        self.shippingRegionId = String()
+    }
+    
+    init(sId: Int, sType: String, sCost: Float, sRegionId: String){
+        self.shippingId = sId
+        self.shippingType = sType
+        self.shippingCost = sCost
+        self.shippingRegionId = sRegionId
+    }
+    
+    func updateShippingInfo(){
+        
+    }
+}
