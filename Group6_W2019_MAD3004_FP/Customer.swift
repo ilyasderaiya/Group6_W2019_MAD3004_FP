2dfa20c0a8ec1c4975fe9cc20be01713cffcba88
Ilyas <ilyasderaiya@gmail.com>
2019-02-21 18:32:45 -0500

    Added Method and Var to Customer class

--- Group6_W2019_MAD3004_FP/Customer.swift
+++ Group6_W2019_MAD3004_FP/Customer.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class Customer{
+    var customerName: String
+    var address: String
+    var email: String
+    var creditCardInfo: String
+    var shippingInfo: String
+    
+    init(){
+        self.customerName = String()
+        self.address = String()
+        self.email = String()
+        self.creditCardInfo = String()
+        self.shippingInfo = String()
+    }
+    
+    init(cusName: String, cusAddress: String, cusEmail: String, ccInfo: String, shipInfo: String) {
+        self.customerName = cusName
+        self.address = cusAddress
+        self.email = cusEmail
+        self.creditCardInfo = ccInfo
+        self.shippingInfo = shipInfo
+    }
+    
+    
+    func register(){
+        
+    }
+    
+    func login(){
+        
+    }
+    
+    func updateProfile(){
+        
+    }
+}
