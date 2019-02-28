5ff01080d1867cf4b0b4d237ef9cff0d4229bfbe
Ilyas <ilyasderaiya@gmail.com>
2019-02-28 18:38:58 -0500

    Unwrapped Customer

--- Group6_W2019_MAD3004_FP/Customer.swift
+++ Group6_W2019_MAD3004_FP/Customer.swift
@@ -87,11 +87,11 @@ class Customer: User, IDisplay {
     
     func displayData(){
         print("--------------Customer Details------------")
-        print("Customer Name: \(String(describing: self.customerName))")
-        print("Customer Address: \(String(describing: self.address))")
-        print("Customer Email: \(String(describing: self.email))")
-        print("Customer Credit Card: \(String(describing: self.creditCardInfo))")
-        print("Shipping: \(String(describing: self.shippingInfo))")
+        print("Customer Name: \(self.customerName!)")
+        print("Customer Address: \(self.address!)")
+        print("Customer Email: \(self.email!)")
+        print("Customer Credit Card: \(self.creditCardInfo!)")
+        print("Shipping: \(self.shippingInfo!)")
     }
     
 }
